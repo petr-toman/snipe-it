8e535df2f66d06683a465a9fb8090933f170d7cd
Petr Toman <petrtoman@ERGO-MacBook-Pro.local>
2020-06-17 09:35:51 +0200

    Init - Fork from snipe/snipe-it

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v4.9.2',
+  'full_app_version' => 'v4.9.2 - build 4352-gec723a3da',
+  'build_version' => '4352',
+  'prerelease_version' => '',
+  'hash_version' => 'gec723a3da',
+  'full_hash' => 'v4.9.2-9-gec723a3da',
+  'branch' => 'master',
+);
